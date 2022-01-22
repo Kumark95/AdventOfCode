363be029da3cf335e3ce300cb59aae6a75248815
Cristhian Rivera <cristhian.rivera@hyds.es>
2022-01-22 19:26:44 +0100

    feat: day 4 solution

---
+++ AdventOfCode2021/Day4/BingoCard.cs
@@ -0,0 +1,67 @@
+﻿namespace AdventOfCode2021.Day4;
+
+public class BingoCard
+{
+    private BingoCardNumber[][] Grid { get; set; }
+
+    public BingoCard(int[][] numbers)
+    {
+        Grid = numbers
+            .Select(
+                row => row
+                    .Select(i => new BingoCardNumber(i))
+                    .ToArray()
+            )
+            .ToArray();
+    }
+
+    public bool MarkNumber(int number)
+    {
+        for (int row = 0; row < Grid.Length; row++)
+        {
+            for (int col = 0; col < Grid[row].Length; col++)
+            {
+                var cardNumber = Grid[row][col];
+                if (cardNumber.Number == number)
+                {
+                    cardNumber.Mark();
+
+                    return Check();
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public int SumNotMarked()
+    {
+        return Grid
+            .SelectMany(l => l)
+            .Where(i => !i.IsMarked)
+            .Select(i => i.Number)
+            .Sum();
+    }
+
+    private bool Check()
+    {
+        for (int row = 0; row < Grid.Length; row++)
+        {
+            var rowResult = true;
+            var colResult = true;
+
+            for (int col = 0; col < Grid[row].Length; col++)
+            {
+                rowResult = rowResult && Grid[row][col].IsMarked;
+                colResult = colResult && Grid[col][row].IsMarked;
+            }
+
+            if (rowResult || colResult)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ AdventOfCode2021/Day4/BingoCardNumber.cs
@@ -0,0 +1,18 @@
+﻿namespace AdventOfCode2021.Day4;
+
+public class BingoCardNumber
+{
+    public bool IsMarked { get; private set; }
+    public int Number { get; private set; }
+
+    public BingoCardNumber(int number)
+    {
+        IsMarked = false;
+        Number = number;
+    }
+
+    public void Mark()
+    {
+        IsMarked = true;
+    }
+}
