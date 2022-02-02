cc22f3650c2722b9cf0b1308f7f55ffac8b36337
Cristhian Rivera <cristhianrivera4086@gmail.com>
2022-02-02 23:39:01 +0100

    feat: day 11

---
+++ Core/Year2021/Day11/DumboOctopus.cs
@@ -0,0 +1,20 @@
+﻿using AdventOfCode.Common.Model;
+
+namespace AdventOfCode.Core.Year2021.Day11;
+
+public class DumboOctopus
+{
+    public Point Location { get; private set; }
+    public int Energy { get; private set; }
+
+    public DumboOctopus(int row, int col, int initialEnergy)
+    {
+        Location = new Point(row, col);
+        Energy = initialEnergy;
+    }
+
+    public void Charge() => Energy++;
+
+    public void Reset() => Energy = 0;
+    public bool IsFlashing() => Energy > 9;
+}
