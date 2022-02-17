ed5f3631ecc18a8615ddb24ab64a3a596b570095
Cristhian Rivera <cristhianrivera4086@gmail.com>
2022-02-17 23:52:55 +0100

    feat: day 16

---
+++ Core/Year2021/Day16/PacketLengthType.cs
@@ -0,0 +1,7 @@
+﻿namespace AdventOfCode.Core.Year2021.Day16;
+
+public enum PacketLengthType
+{
+    TotalSubPacketLength = 0,
+    NumberOfSubPackets = 1
+}
---
+++ Core/Year2021/Day16/PuzzleSolver.cs
@@ -0,0 +1,48 @@
+﻿using AdventOfCode.Common.Attributes;
+using AdventOfCode.Common.Interfaces;
+
+namespace AdventOfCode.Core.Year2021.Day16;
+
+[PuzzleName("Packet Decoder")]
+public class PuzzleSolver : IPuzzleSolver
+{
+    public int Year => 2021;
+    public int Day => 16;
+
+    /// <summary>
+    /// Returns the binary representation of the input
+    /// </summary>
+    /// <remarks>
+    /// Padding is used to make each hex digit a binary representation with width 4
+    /// </remarks>
+    /// <param name="inputLines"></param>
+    /// <returns></returns>
+    private static string Input(string[] inputLines)
+    {
+        return inputLines[0]
+            .Select(c =>
+            {
+                var dec = Convert.ToInt32(c.ToString(), fromBase: 16);
+                var binary = Convert.ToString(dec, toBase: 2);
+
+                return binary.PadLeft(totalWidth: 4, paddingChar: '0');
+            })
+            .Aggregate((s1, s2) => s1 + "" + s2);
+    }
+
+    public long SolvePartOne(string[] inputLines)
+    {
+        var input = Input(inputLines);
+
+        var packet = Packet.Decode(input);
+        return Packet.SumVersions(packet);
+    }
+
+    public long? SolvePartTwo(string[] inputLines)
+    {
+        var input = Input(inputLines);
+
+        var packet = Packet.Decode(input);
+        return packet.Value;
+    }
+}
