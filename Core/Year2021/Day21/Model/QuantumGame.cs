307364eb223787ddc78d6b64624ac44d97d19be2
Cristhian Rivera <cristhianrivera4086@gmail.com>
2022-08-25 18:56:30 +0200

    feat: Year 2021 Day 21

---
+++ Core/Year2021/Day21/Model/QuantumGame.cs
@@ -0,0 +1,152 @@
+﻿namespace AdventOfCode.Core.Year2021.Day21.Model;
+
+internal class QuantumGame
+{
+    private int MaxScore { get; init; }
+    private Dictionary<GameState, QuantumGameResult> ResultCache { get; set; }
+    private readonly Dictionary<int, int> DiracDieResultFrequency = new()
+    {
+        { 3, 1 },
+        { 4, 3 },
+        { 5, 6 },
+        { 6, 7 },
+        { 7, 6 },
+        { 8, 3 },
+        { 9, 1 }
+    };
+
+    public QuantumGame(int maxScore)
+    {
+        MaxScore = maxScore;
+        ResultCache = new Dictionary<GameState, QuantumGameResult>();
+    }
+
+    private static int ChangePlayerPosition(int initialPosition, int increment)
+    {
+        // Subtract 1 before modulo. Add 1 after as the numbers start from 1 not 0
+        return (initialPosition + increment - 1) % 10 + 1;
+    }
+
+    /// <summary>
+    /// Play the game with a given state
+    /// </summary>
+    /// <param name="state"></param>
+    /// <returns>The number of times each player has won</returns>
+    public QuantumGameResult Play(GameState state)
+    {
+        if (ResultCache.ContainsKey(state))
+        {
+            return ResultCache[state];
+        }
+
+        long player1Victories = 0;
+        long player2Victories = 0;
+
+        // TODO: Refactor
+        if (state.IsPlayer1Turn)
+        {
+            foreach (var (positionIncrease, dieResultFrequency) in DiracDieResultFrequency)
+            {
+                var landingPosition = ChangePlayerPosition(state.PlayerAPosition, positionIncrease);
+                var playerNewScore = state.PlayerAScore + landingPosition;
+
+                var newGameState = state with
+                {
+                    PlayerAPosition = landingPosition,
+                    PlayerAScore = playerNewScore,
+                    IsPlayer1Turn = false,
+                };
+
+                QuantumGameResult newStateResults;
+                if (ResultCache.ContainsKey(newGameState))
+                {
+                    var intermediateResults = ResultCache[newGameState];
+                    newStateResults = intermediateResults with
+                    {
+                        PlayerAVictories = intermediateResults.PlayerAVictories * dieResultFrequency,
+                        PlayerBVictories = intermediateResults.PlayerBVictories * dieResultFrequency
+                    };
+                }
+                else if (playerNewScore >= MaxScore)
+                {
+                    newStateResults = new QuantumGameResult(PlayerAVictories: dieResultFrequency, PlayerBVictories: 0);
+                }
+                else
+                {
+                    var intermediateResults = Play(newGameState);
+                    newStateResults = intermediateResults with
+                    {
+                        PlayerAVictories = intermediateResults.PlayerAVictories * dieResultFrequency,
+                        PlayerBVictories = intermediateResults.PlayerBVictories * dieResultFrequency
+                    };
+                }
+
+                player1Victories += newStateResults.PlayerAVictories;
+                player2Victories += newStateResults.PlayerBVictories;
+            }
+        }
+        else
+        {
+            foreach (var (positionIncrease, dieResultFrequency) in DiracDieResultFrequency)
+            {
+                var landingPosition = ChangePlayerPosition(state.PlayerBPosition, positionIncrease);
+                var playerNewScore = state.PlayerBScore + landingPosition;
+
+                var newGameState = state with
+                {
+                    PlayerBPosition = landingPosition,
+                    PlayerBScore = playerNewScore,
+                    IsPlayer1Turn = true,
+                };
+
+                QuantumGameResult newStateResults;
+                if (ResultCache.ContainsKey(newGameState))
+                {
+                    var intermediateResults = ResultCache[newGameState];
+                    newStateResults = intermediateResults with
+                    {
+                        PlayerAVictories = intermediateResults.PlayerAVictories * dieResultFrequency,
+                        PlayerBVictories = intermediateResults.PlayerBVictories * dieResultFrequency
+                    };
+                }
+                else if (playerNewScore >= MaxScore)
+                {
+                    newStateResults = new QuantumGameResult(PlayerAVictories: 0, PlayerBVictories: dieResultFrequency);
+                }
+                else
+                {
+                    var intermediateResults = Play(newGameState);
+                    newStateResults = intermediateResults with
+                    {
+                        PlayerAVictories = intermediateResults.PlayerAVictories * dieResultFrequency,
+                        PlayerBVictories = intermediateResults.PlayerBVictories * dieResultFrequency
+                    };
+                }
+
+                player1Victories += newStateResults.PlayerAVictories;
+                player2Victories += newStateResults.PlayerBVictories;
+            }
+        }
+
+        var result = new QuantumGameResult
+        {
+            PlayerAVictories = player1Victories,
+            PlayerBVictories = player2Victories
+        };
+
+        ResultCache.Add(state, result);
+        return result;
+    }
+}
+
+
+internal record struct GameState
+{
+    public int PlayerAPosition { get; init; }
+    public int PlayerAScore { get; init; }
+    public int PlayerBPosition { get; init; }
+    public int PlayerBScore { get; init; }
+    public bool IsPlayer1Turn { get; init; }
+}
+
+internal record struct QuantumGameResult(long PlayerAVictories, long PlayerBVictories);
---
+++ Core/Year2021/Day21/PuzzleSolver.cs
@@ -0,0 +1,55 @@
+﻿using AdventOfCode.Common.Attributes;
+using AdventOfCode.Common.Interfaces;
+using AdventOfCode.Core.Year2021.Day21.Model;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Core.Year2021.Day21;
+
+[PuzzleName("Dirac Dice")]
+public class PuzzleSolver : IPuzzleSolver
+{
+    public int Year => 2021;
+    public int Day => 21;
+
+    private static List<Player> Input(string[] inputLines)
+    {
+        var playerPositions = new List<Player>();
+        foreach (var line in inputLines)
+        {
+            var matches = Regex.Match(line, @"Player (?<PlayerId>\d+) starting position: (?<InitialPosition>\d+)");
+            var playerId = int.Parse(matches.Groups["PlayerId"].Value);
+            var initialPosition = int.Parse(matches.Groups["InitialPosition"].Value);
+
+            playerPositions.Add(new Player(playerId, initialPosition));
+        }
+
+        return playerPositions;
+    }
+
+    public long SolvePartOne(string[] inputLines)
+    {
+        var players = Input(inputLines);
+        var game = new PracticeGame(maxScore: 1000);
+
+        var result = game.Play(players);
+        return result.LosingPlayerScore * result.DieRollCount;
+    }
+
+    public long? SolvePartTwo(string[] inputLines)
+    {
+        var players = Input(inputLines);
+
+        var quantumGame = new QuantumGame(maxScore: 21);
+
+        var initialGameState = new GameState
+        {
+            PlayerAPosition = players[0].Position,
+            PlayerAScore = 0,
+            PlayerBPosition = players[1].Position,
+            PlayerBScore = 0,
+            IsPlayer1Turn = true
+        };
+        var result = quantumGame.Play(initialGameState);
+        return result.PlayerAVictories > result.PlayerBVictories ? result.PlayerAVictories : result.PlayerBVictories;
+    }
+}
