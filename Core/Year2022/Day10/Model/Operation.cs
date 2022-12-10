7533c940025a6ac0ec1a01ffa76b21b36774d43d
Cristhian Rivera <cristhianrivera4086@gmail.com>
2022-12-10 19:39:35 +0100

    feat: Year 2022 Day 10

---
+++ Core/Year2022/Day10/Model/Operation.cs
@@ -0,0 +1,7 @@
+namespace AdventOfCode.Core.Year2022.Day10.Model;
+
+internal static class Operation
+{
+    public static readonly string NoOperation = "noop";
+    public static readonly string Add = "addx";
+}
