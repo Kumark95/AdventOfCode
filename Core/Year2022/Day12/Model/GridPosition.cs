cf0201e891fed362d11191639496c0705c7503a3
Cristhian Rivera <cristhianrivera4086@gmail.com>
2022-12-14 23:23:24 +0100

    feat: Year 2022 Day 12

---
+++ Core/Year2022/Day12/Model/GridPosition.cs
@@ -0,0 +1,3 @@
+namespace AdventOfCode.Core.Year2022.Day12.Model;
+
+internal readonly record struct GridPosition(int Row, int Col);
---
+++ Core/Year2022/Day12/PuzzleSolver.cs
@@ -0,0 +1,40 @@
+using AdventOfCode.Common.Attributes;
+using AdventOfCode.Common.Interfaces;
+using AdventOfCode.Core.Year2022.Day12.Model;
+
+namespace AdventOfCode.Core.Year2022.Day12;
+
+[PuzzleName("Hill Climbing Algorithm")]
+public class PuzzleSolver : IPuzzleSolver
+{
+    public int Year => 2022;
+    public int Day => 12;
+
+    public long SolvePartOne(string[] inputLines)
+    {
+        var maze = Maze.Parse(inputLines);
+        return maze.FindShortestDistance(maze.StartPosition, maze.EndPosition);
+    }
+
+    public long? SolvePartTwo(string[] inputLines)
+    {
+        var maze = Maze.Parse(inputLines);
+
+        var lowPositionDistances = new List<int>();
+        foreach (var lowPosition in maze.LowPositions)
+        {
+            maze.Reset();
+
+            var shortestDistance = maze.FindShortestDistance(lowPosition, maze.EndPosition);
+            if (shortestDistance == -1)
+            {
+                continue;
+            }
+
+            lowPositionDistances.Add(shortestDistance);
+        }
+
+        return lowPositionDistances.Min();
+    }
+}
+
