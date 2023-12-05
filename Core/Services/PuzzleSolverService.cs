b62726da5615b3624f6430aa9a7dda3106462511
Cristhian Rivera <cristhianrivera4086@gmail.com>
2023-12-05 20:14:07 +0100

    refactor: unify the profiling methods

--- Core/Services/PuzzleSolverService.cs
+++ Core/Services/PuzzleSolverService.cs
@@ -26,12 +26,10 @@ internal sealed class PuzzleSolverService
         }
 
         // Start
-        var timer = new Stopwatch();
-
-        foreach (var sampelFile in _sampleFiles)
+        foreach (var sampleFile in _sampleFiles)
         {
-            var sampleFilePath = Path.Combine(solverDirectory, sampelFile);
-            _logger.LogInformation("Using {SampleFile}", sampelFile);
+            var sampleFilePath = Path.Combine(solverDirectory, sampleFile);
+            _logger.LogInformation("Using {SampleFile}", sampleFile);
             if (!File.Exists(sampleFilePath))
             {
                 _logger.LogWarning("Input file {SampleFilePath} does not exists. Skipping...", sampleFilePath);
@@ -40,25 +38,8 @@ internal sealed class PuzzleSolverService
 
             var inputContent = File.ReadAllLines(sampleFilePath);
 
-            timer.Restart();
-            var resultPartOne = solver.SolvePartOne(inputContent);
-            timer.Stop();
-            _logger.LogInformation("Part one result: {Result} | Took: {ElapsedSeconds}s {ElapsedMilliseconds}ms {ElapsedMicroseconds}us",
-                resultPartOne, timer.Elapsed.TotalSeconds, timer.Elapsed.Milliseconds, timer.Elapsed.Microseconds);
-
-            timer.Restart();
-            var resultPartTwo = solver.SolvePartTwo(inputContent);
-            timer.Stop();
-
-            if (resultPartTwo is null)
-            {
-                _logger.LogInformation("Part two not yet unlocked");
-            }
-            else
-            {
-                _logger.LogInformation("Part two result: {Result} | Took: {ElapsedSeconds}s {ElapsedMilliseconds}ms {ElapsedMicroseconds}us",
-                    resultPartTwo, timer.Elapsed.TotalSeconds, timer.Elapsed.Milliseconds, timer.Elapsed.Microseconds);
-            }
+            MeasurePerformance(() => solver.SolvePartOne(inputContent), "PartOne");
+            MeasurePerformance(() => solver.SolvePartTwo(inputContent), "PartTwo");
         }
     }
 
@@ -73,5 +54,55 @@ internal sealed class PuzzleSolverService
 
         // Select target solver
         return solvers.SingleOrDefault(s => s.Year == year && s.Day == day);
+    }
+
+    private void MeasurePerformance<T>(Func<T> func, string funcName)
+    {
+        var startMemory = 0L;
+        using (Process proc = Process.GetCurrentProcess())
+        {
+            startMemory = Process.GetCurrentProcess().PrivateMemorySize64;
+        }
+        var startTimestamp = Stopwatch.GetTimestamp();
+        var result = func();
+        var elapsed = Stopwatch.GetElapsedTime(startTimestamp);
+        var endMemory = 0L;
+        using (Process proc = Process.GetCurrentProcess())
+        {
+            endMemory = Process.GetCurrentProcess().PrivateMemorySize64;
+        }
+
+        if (result is null)
+        {
+            _logger.LogInformation("{FuncName} not yet unlocked", funcName);
+        }
+        else
+        {
+            var elapsedMessage = elapsed.Minutes > 0
+                ? $"{elapsed.Minutes}m {elapsed.Seconds}s {elapsed.Milliseconds}ms {elapsed.Microseconds}μs"
+                : $"{elapsed.Seconds}s {elapsed.Milliseconds}ms {elapsed.Microseconds}μs";
+
+            _logger.LogInformation("{FuncName} result: {Result} | Took: {Elapsed} | Memory: {MemoryUsed}",
+                funcName, result, elapsedMessage, FormatBytes(endMemory - startMemory));
+        }
+    }
+
+    private static string FormatBytes(long bytes)
+    {
+        const int scale = 1024;
+        string[] orders = ["GB", "MB", "KB", "Bytes"];
+        long max = (long)Math.Pow(scale, orders.Length - 1);
+
+        foreach (string order in orders)
+        {
+            if (bytes > max)
+            {
+                return string.Format("{0:##.##} {1}", decimal.Divide(bytes, max), order);
+            }
+
+            max /= scale;
+        }
+
+        return "0 Bytes";
     }
 }
--- Core/Solvers/Year2023/Day05/PuzzleSolver.cs
+++ Core/Solvers/Year2023/Day05/PuzzleSolver.cs
@@ -4,6 +4,8 @@ using AdventOfCode.Core.Solvers.Year2023.Day05.Model;
 
 namespace AdventOfCode.Core.Solvers.Year2023.Day05;
 
+public record R(long SourceStart, long SourceEnd, long DestinationStart, long DestinationEnd);
+
 [PuzzleName("If You Give A Seed A Fertilizer")]
 public sealed class PuzzleSolver : IPuzzleSolver
 {
@@ -14,6 +16,63 @@ public sealed class PuzzleSolver : IPuzzleSolver
     {
         var seedNumbers = Almanac.ParseSeedNumbers(inputLines[0]);
         var converters = Almanac.ParseConverters(inputLines[2..]);
+
+
+        var i = 0;
+        foreach (var cv in converters)
+        {
+            Console.WriteLine($"Converter {i}");
+            i++;
+
+            //var rr = cv.Ranges
+            //    .OrderBy(r => r.DestinationStart)
+            //    .Select(r => new R(r.SourceStart, r.SourceStart + r.Length - 1, r.DestinationStart, r.DestinationStart + r.Length - 1))
+            //    .ToList();
+
+            //var prev = rr.First();
+            //Console.WriteLine($"R [{prev.DestinationStart}, {prev.DestinationEnd}]");
+            //foreach (var r in rr.Skip(1))
+            //{
+            //    Console.WriteLine($"R [{r.DestinationStart}, {r.DestinationEnd}]");
+
+            //    if (r.DestinationStart != prev.DestinationEnd + 1)
+            //    {
+            //        Console.WriteLine($"\t -> Range not contiguous");
+            //    }
+
+            //    prev = r;
+            //}
+
+            var rr = cv.Ranges
+                .OrderBy(r => r.SourceStart)
+                .Select(r => new R(r.SourceStart, r.SourceStart + r.Length - 1, r.DestinationStart, r.DestinationStart + r.Length - 1))
+                .ToList();
+
+            var prev = rr.First();
+            Console.WriteLine($"R [{prev.SourceStart}, {prev.SourceEnd}]");
+            foreach (var r in rr.Skip(1))
+            {
+                Console.WriteLine($"R [{r.SourceStart}, {r.SourceEnd}]");
+
+                if (r.SourceStart != prev.SourceEnd + 1)
+                {
+                    Console.WriteLine($"\t -> Range not contiguous");
+                }
+
+                prev = r;
+            }
+
+            //foreach (var r in rr)
+            //{
+            //    var sF = r.SourceStart;
+            //    var sT = r.SourceStart + r.Length - 1;
+
+            //    var dF = r.DestinationStart;
+            //    var dT = r.DestinationStart + r.Length - 1;
+            //    Console.WriteLine($"FROM [{sF},{sT}] TO [{dF},{dT}]");
+            //}
+        }
+
 
         var minConvertedValue = long.MaxValue;
         foreach (var number in seedNumbers)
@@ -26,6 +85,8 @@ public sealed class PuzzleSolver : IPuzzleSolver
 
     public long? SolvePartTwo(string[] inputLines)
     {
+        return null;
+
         var seedRanges = Almanac.ParseSeedNumbers(inputLines[0]);
         var converters = Almanac.ParseConverters(inputLines[2..]);
 
