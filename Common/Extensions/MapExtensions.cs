e0e03d27467c3db341bed7be853dffd9998e7730
Cristhian Rivera <cristhianrivera4086@gmail.com>
2023-12-10 13:36:54 +0100

    change: update MapExtensions to take Position as parameters

--- Common/Extensions/MapExtensions.cs
+++ Common/Extensions/MapExtensions.cs
@@ -12,7 +12,15 @@ public static class MapExtensions
             && col < map.GetLength(1);
     }
 
-    public static IEnumerable<(Position pos, T value)> GetAllNeighbours<T>(this T[,] map, int row, int col, MapConnectivity connectivity)
+    public static bool IsValidPosition<T>(this T[,] map, Position position)
+    {
+        return position.Row >= 0
+            && position.Row < map.GetLength(0)
+            && position.Col >= 0
+            && position.Col < map.GetLength(1);
+    }
+
+    public static IEnumerable<(Position Position, T Value)> GetAllNeighbours<T>(this T[,] map, int row, int col, MapConnectivity connectivity)
     {
         int[] rowDirection;
         int[] colDirection;
@@ -39,6 +47,35 @@ public static class MapExtensions
 
             var adjCharacter = map[adjRow, adjCol];
             yield return (new Position(adjRow, adjCol), adjCharacter);
+        }
+    }
+
+    public static IEnumerable<(Position Position, T Value)> GetAllNeighbours<T>(this T[,] map, Position position, MapConnectivity connectivity)
+    {
+        int[] rowDirection;
+        int[] colDirection;
+
+        if (connectivity == MapConnectivity.EightConnected)
+        {
+            rowDirection = [-1, 0, 1, 0, -1, 1, -1, 1];
+            colDirection = [0, 1, 0, -1, -1, -1, 1, 1];
+        }
+        else
+        {
+            rowDirection = [-1, 0, 1, 0];
+            colDirection = [0, 1, 0, -1];
+        }
+
+        for (int i = 0; i < rowDirection.Length; i++)
+        {
+            var adjPosition = new Position(position.Row + rowDirection[i], position.Col + colDirection[i]);
+            if (!map.IsValidPosition(adjPosition))
+            {
+                continue;
+            }
+
+            var adjCharacter = map[adjPosition.Row, adjPosition.Col];
+            yield return (adjPosition, adjCharacter);
         }
     }
 }
