340e1ae20aed2906ae0571fa979a3d1781ad6563
Cristhian Rivera <cristhianrivera4086@gmail.com>
2023-12-17 17:51:52 +0100

    change: update tempalte to inclue Input attribute

--- Core/Services/PuzzleSetupService.cs
+++ Core/Services/PuzzleSetupService.cs
@@ -32,6 +32,9 @@ internal sealed class PuzzleSetupService
         File.WriteAllText(Path.Combine(solverDirectory, "example.txt"), string.Empty);
         File.WriteAllText(Path.Combine(solverDirectory, "input.txt"), await _adventOfCodeService.GetPuzzleInput(year, day));
         File.WriteAllText(Path.Combine(solverDirectory, "PuzzleSolver.cs"), GenerateSolver(year, day, puzzleName));
+
+        Directory.CreateDirectory(Path.Combine(solverDirectory, "Model"));
+        File.WriteAllText(Path.Combine(solverDirectory, "Model", "InputParser.cs"), GenerateParser(year, day));
     }
 
     private static string GenerateSolver(int year, int day, string puzzleName)
@@ -39,6 +42,7 @@ internal sealed class PuzzleSetupService
         return $$"""
             using AdventOfCode.Common.Attributes;
             using AdventOfCode.Common.Interfaces;
+            using AdventOfCode.Core.Year{{year:D4}}.Day{{day:D2}}.Model;
 
             namespace AdventOfCode.Core.Year{{year:D4}}.Day{{day:D2}};
 
@@ -48,17 +52,48 @@ internal sealed class PuzzleSetupService
                 public int Year => {{year}};
                 public int Day => {{day}};
 
+                [PuzzleInput(filename: "example.txt", expectedResult: -1)]
+                [PuzzleInput(filename: "input.txt", expectedResult: -1)]
                 public long? SolvePartOne(string[] inputLines)
                 {
                     return 0;
                 }
 
+                [PuzzleInput(filename: "example.txt", expectedResult: -1)]
+                [PuzzleInput(filename: "input.txt", expectedResult: -1)]
                 public long? SolvePartTwo(string[] inputLines)
                 {
                     return null;
                 }
             }
 
+            """;
+    }
+
+    private static string GenerateParser(int year, int day)
+    {
+        return $$"""
+            namespace AdventOfCode.Core.Year{{year:D4}}.Day{{day:D2}}.Model;
+
+            internal static class InputParser
+            {
+                public static char[,] ParseInput(string[] inputLines)
+                {
+                    var rowLength = inputLines.Length;
+                    var colLength = inputLines[0].Length;
+                    var map = new char[rowLength, colLength];
+
+                    for (var row = 0; row < rowLength; row++)
+                    {
+                        for (var col = 0; col < colLength; col++)
+                        {
+                            map[row, col] = inputLines[row][col];
+                        }
+                    }
+
+                    return map;
+                }
+            }
             """;
     }
 }
