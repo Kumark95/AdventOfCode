7e6fc898ca07b74df5ae00d09c97d7b3dd20989d
Cristhian Rivera <cristhianrivera4086@gmail.com>
2023-12-30 16:44:27 +0100

    change: add the direction increments to the DirectionExtensions

--- Common/Extensions/DirectionExtensions.cs
+++ Common/Extensions/DirectionExtensions.cs
@@ -4,6 +4,14 @@ namespace AdventOfCode.Common.Extensions;
 
 public static class DirectionExtensions
 {
+    private static readonly Dictionary<Direction, Position> _dirIncrements = new()
+    {
+        { Direction.Up, new(-1, 0) },
+        { Direction.Down, new(1, 0) },
+        { Direction.Left, new(0, -1) },
+        { Direction.Right, new(0, 1) },
+    };
+
     public static bool IsOposite(this Direction baseDir, Direction targetDir)
     {
         return (baseDir, targetDir) switch
@@ -14,5 +22,10 @@ public static class DirectionExtensions
             (Direction.Right, Direction.Left) => true,
             _ => false
         };
+    }
+
+    public static Position DirectionIncrement(this Direction direction)
+    {
+        return _dirIncrements[direction];
     }
 }
